f7e9bdf2942fd8d6f4d57f79f82e95c9af2f0e8d
marcosdima <marcosdimatteo.wgr@gmail.com>
2026-06-01 15:08:59 -0300

    Post model

--- micro-reddit/db/schema.rb
+++ micro-reddit/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_01_175748) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_01_180515) do
+  create_table "posts", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "alias"
     t.datetime "created_at", null: false
