030b47c5f2b05417d356ea4cb395ac6e0333e93b
marcosdima <marcosdimatteo.wgr@gmail.com>
2026-06-01 15:12:02 -0300

    post belongs to user

--- micro-reddit/db/schema.rb
+++ micro-reddit/db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_01_180515) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_01_180924) do
   create_table "posts", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.string "title"
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -24,4 +26,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_01_180515) do
     t.datetime "updated_at", null: false
     t.string "username"
   end
+
+  add_foreign_key "posts", "users"
 end
