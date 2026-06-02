cd826c347d8bc5562e4decddcd20005d07b03c39
marcosdima <marcosdimatteo.wgr@gmail.com>
2026-06-02 13:22:20 -0300

    implement create

--- re-former/app/controllers/users_controller.rb
+++ re-former/app/controllers/users_controller.rb
@@ -3,5 +3,15 @@ class UsersController < ApplicationController
   end
   
   def create
+    @user = User.new(user_params)
+    if @user.save
+      render :new
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def user_params
+    params.expect(user: [:username, :email, :password])
   end
 end
