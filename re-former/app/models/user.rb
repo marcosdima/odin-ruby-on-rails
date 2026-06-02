9d9d130438f815f5237b77c4d989b2ea123eb33a
marcosdima <marcosdimatteo.wgr@gmail.com>
2026-06-02 11:52:15 -0300

    implement User

--- re-former/app/models/user.rb
+++ re-former/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, presence: true, length: { minimum: 2, maximum: 100 }
+  validates :email, presence: true, uniqueness: true, length: { maximum: 255 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 6 }
 end
