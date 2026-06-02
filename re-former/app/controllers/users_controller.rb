fe9f9eefe180110404540085f152648eb92d9b25
marcosdima <marcosdimatteo.wgr@gmail.com>
2026-06-02 14:17:23 -0300

    updating new

--- re-former/app/controllers/users_controller.rb
+++ re-former/app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
   
   def create
     @user = User.new(user_params)
     if @user.save
-      render :new
+      redirect_to new_users_path
     else
       render :new, status: :unprocessable_entity
     end
