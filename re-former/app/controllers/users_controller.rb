916ab78f16af9bbcba2faaa1d9f54c8149e80376
marcosdima <marcosdimatteo.wgr@gmail.com>
2026-06-02 22:16:41 -0300

    Complete routes

--- re-former/app/controllers/users_controller.rb
+++ re-former/app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show update edit ]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
   def new
     @user = User.new
   end
@@ -6,13 +15,29 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to new_users_path
+      redirect_to @user
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  def user_params
-    params.expect(user: [:username, :email, :password])
+  def edit
   end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def user_params
+      params.expect(user: [:username, :email, :password])
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
--- re-former/config/routes.rb
+++ re-former/config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resource :users, only: [:new, :create]
+  resources :users
 end
