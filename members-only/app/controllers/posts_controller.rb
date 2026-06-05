ec43e1434f44697d1b55f9183087812b18a2e355
marcosdima <marcosdimatteo.wgr@gmail.com>
2026-06-05 15:59:45 -0300

    index and new post views

--- members-only/app/controllers/posts_controller.rb
+++ members-only/app/controllers/posts_controller.rb
@@ -1,11 +1,28 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [ :new, :create ]
   def new
+    @post = Post.new
   end
 
   def index
+    @posts = Post.all
   end
 
   def create
+    @post = current_user.posts.create(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new, status: unprocessable_entity
+    end
   end
+
+  private
+    def post_params
+      params.expect(post: [ :title, :content ])
+    end
+
+    def set_post
+      @post = Post.find(params[:id])
+    end
 end
