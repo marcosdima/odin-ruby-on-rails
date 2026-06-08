e496fff158836d179fa098439f4ed36c386290b3
marcosdima <marcosdimatteo.wgr@gmail.com>
2026-06-08 08:51:51 -0300

    add username migration

---
+++ private-events/db/migrate/20260608114452_add_username_to_user.rb
@@ -0,0 +1,8 @@
+class AddUsernameToUser < ActiveRecord::Migration[8.1]
+  def change
+    change_table :users do |t|
+      t.string :username
+    end
+    add_index :users, :username, unique: true
+  end
+end
